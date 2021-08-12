4295cdd672583db4a5ef9639e199880084d65c1c
hochan222 <hochan049@gmail.com>
2021-08-12 22:50:23 +0900

    test: husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
     ecmaVersion: 12,
     sourceType: 'module',
   },
