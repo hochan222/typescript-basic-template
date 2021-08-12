14a03f4713824f52db5cd826e4f5fcbf9f6edaa0
hochan222 <hochan049@gmail.com>
2021-08-12 21:18:15 +0900

    chore: add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const button = document.getElementById("button");
+const button = document.getElementById('button');
 
-button?.addEventListener("click", () => {
-  alert("clicked!");
+button?.addEventListener('click', () => {
+  alert('clicked!');
 });
