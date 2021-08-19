6b90e109577e6cabb5df3493e6b2195896745bee
hochan222 <hochan049@gmail.com>
2021-08-19 20:16:15 +0900

    feat: allowUnreachableCode, allowUnusedLabels, exactOptionalPropertyTypes options '

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,39 @@ const button = document.getElementById('button');
 button?.addEventListener('click', () => {
   button.innerText = 'holee-button';
 });
+
+/* 
+** "allowUnreachableCode": false,
+*/
+
+// function fn(n: number) {
+//   if (n > 5) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+//   return true;
+// }
+
+/* 
+** "allowUnusedLabels": false,
+*/
+
+// function verifyAge(age: number) {
+//   // Forgot 'return' statement
+//   if (age > 18) {
+//     verified: true;
+//   }
+// }
+
+/* 
+** "exactOptionalPropertyTypes": true,
+*/
+
+// interface UserDefaults {
+//   color?: "dark" | "light";
+// }
+
+// const user: UserDefaults = {
+//   color: undefined, // dark | light | undefined
+// };
