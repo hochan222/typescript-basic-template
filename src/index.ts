eedae7cb0eb6ba553d5ca95c3c0f34463ec83dfc
hochan222 <hochan049@gmail.com>
2021-08-20 02:05:47 +0900

    feat: strictPropertyInitialization options

--- src/index.ts
+++ src/index.ts
@@ -255,3 +255,17 @@
 //     // Note that this.email is not set
 //   }
 // }
+
+/*
+ ** "strictPropertyInitialization": true,
+ */
+
+// try {
+//   // ...
+// } catch (err) {
+//   // We have to verify err is an
+//   // error before using it as one.
+//   if (err instanceof Error) {
+//     console.log(err.message);
+//   }
+// }
