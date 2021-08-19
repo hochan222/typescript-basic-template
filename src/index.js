1f7575d2d8590872cb21432f3b6317cf559216df
hochan222 <hochan049@gmail.com>
2021-08-19 22:19:23 +0900

    feat: noImplicitAny, noImplicitOverride, noImplicitReturns, alwaysStrict options

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 // const button = document.getElementById('button');
-
 // button?.addEventListener('click', () => {
 //   button.innerText = 'holee-button';
 // });
-
 /*
  ** "allowUnreachableCode": false,
  */
-
 // function fn(n: number) {
 //   if (n > 5) {
 //     return true;
@@ -16,36 +13,28 @@
 //   }
 //   return true;
 // }
-
 /*
  ** "allowUnusedLabels": false,
  */
-
 // function verifyAge(age: number) {
 //   // Forgot 'return' statement
 //   if (age > 18) {
 //     verified: true;
 //   }
 // }
-
 /*
  ** "exactOptionalPropertyTypes": true,
  */
-
 // interface UserDefaults {
 //   color?: "dark" | "light";
 // }
-
 // const user: UserDefaults = {
 //   color: undefined, // dark | light | undefined
 // };
-
 /*
  ** "noFallthroughCasesInSwitch": true,
  */
-
 // const fallthrough = 6;
-
 // switch (fallthrough) {
 //   case 1:
 //     // Fallthrough case in switch.ts(7029)
@@ -56,37 +45,29 @@
 //   default:
 //     break;
 // }
-
 /*
  ** "noImplicitAny": true,
  */
-
 // const anyFunction = (num) => {
 //   return num;
 // };
 // anyFunction(42);
-
 /*
  ** "noImplicitOverride": true,
  */
-
 // class weapon {
 //   create() {}
 //   attack() {}
 // }
-
 // class gun extends weapon {
 //   // This member must have an 'override' modifier because it overrides a member in the base class 'weapon'.ts(4114)
 //   create() {}
-
 //   // overriding 키워드를 사용한 명시적 오버라이딩은 허용된다.
 //   override attack() {}
 // }
-
 /*
  ** "noImplicitReturns": true,
  */
-
 // Function lacks ending return statement and return type does not include 'undefined'.ts(2366)
 // function lookupHeadphonesManufacturer(color: 'blue' | 'black'): string {
 //   if (color === 'blue') {
@@ -95,13 +76,7 @@
 //     ('bose');
 //   }
 // }
-
 /*
  ** "alwaysStrict": true,
  */
-
 // const number = 023;
-
-/*
- ** "noImplicitAny": true,
- */
